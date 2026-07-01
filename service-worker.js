91ed32b23421cfb3dc71d68775afecfcc05c397e
MomiaTk <panadeotro@gmail.com>
2026-07-01 17:58:15 -0600

    para offline mode

--- service-worker.js
+++ service-worker.js
@@ -3,6 +3,7 @@ self.addEventListener("install", (event) => {
     console.log("Service Worker instalado");
 
 });
+const CACHE_NAME = "heywaiter-v1";
 
 self.addEventListener("activate", (event) => {
 
